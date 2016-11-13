3eabb1960a488e83bd5ad24b1364265f7225aacb
Petr Kubeš <petrkubes1997@gmail.com>
2016-11-13 16:54:06 +0100

    Update all debts at once

--- app/ApiModule/presenters/DebtsPresenter.php
+++ app/ApiModule/presenters/DebtsPresenter.php
@@ -16,11 +16,117 @@ class DebtsPresenter extends BaseApiPresenter {
 	}
 
 	/**
-	 * Responses with a list of users depts
+	 * Responses with a list of user's depts
 	 * @param null $which
 	 */
 	public function actionDefault($which = null) {
+		$this->sendSuccessResponse($this->getDebtsArray($which));
+	}
 
+	/**
+	 * Updates newest debts and shows debts's most recent versions
+	 */
+	public function actionUpdate() {
+
+		$allReceivedDebts = json_decode($this->request->getRawBody(), true);
+
+		// Go through each debt saved on a mobile device right now, update the database, and return all debts
+		foreach ($allReceivedDebts['debts'] as $receivedDebt) {
+
+
+			// Check the most important things which are nessesary for further actions
+			try {
+				$id = $receivedDebt['id'];
+				// If id is < than 0, it means that it's a new debt and a new row must be created
+				if (!isset($id) || $id < 0) {
+					$debt = new Debt();
+				} else {
+					$debt = $this->orm->debts->getById($id);
+				}
+
+				if ($debt === null) {
+					throw new Exception('This debt does not exist.');
+				}
+
+				if ($receivedDebt['createdAt'] == "") {
+					throw new Exception('CreatedAt has to be set.');
+				}
+
+				if ($receivedDebt['modifiedAt'] == "") {
+					throw new Exception('ModifiedAt has to be set.');
+				}
+
+				if ($receivedDebt['version'] == "") {
+					throw new Exception('Version has to be an int.');
+				}
+
+			} catch (Exception $e) {
+				$this->sendErrorResponse('Debt ' . $id . ': ' . $e->getMessage());
+			}
+
+
+			// Check if the recieved debt is the most recent version
+			if ($id > 0) {
+				// If the recieved debt is older, send newest version
+				// in case of versions being equal, debt won't be updated
+				if ((int)$receivedDebt['version'] <= $debt->version) {
+					continue;
+				}
+			}
+
+			$paidAt = null;
+			$deletedAt = null;
+			if ($receivedDebt['paidAt'] != "") {
+				$paidAt = DateTime::from($receivedDebt['paidAt']);
+			}
+
+			if ($receivedDebt['deletedAt'] != "") {
+				$deletedAt = DateTime::from($receivedDebt['deletedAt']);
+			}
+
+			// Here we have the newest debt, let's update the databse
+
+			$debt->creditor = $this->orm->users->getById($receivedDebt['creditorId']);
+			$debt->debtor = $this->orm->users->getById($receivedDebt['debtorId']);
+			$debt->customFriendName = $receivedDebt['customFriendName'];
+			$debt->amount = $receivedDebt['amount'];
+			$debt->currency = $this->orm->currencies->getById($receivedDebt['currencyId']);
+			$debt->thingName = $receivedDebt['thingName'];
+			$debt->note = $receivedDebt['note'];
+			$debt->paidAt = $paidAt;
+			$debt->deletedAt = $deletedAt;
+			$debt->createdAt = DateTime::from($receivedDebt['createdAt']);
+			$debt->modifiedAt = new DateTime();
+			$debt->version = (int)$receivedDebt['version'];
+
+			// Check if the debt is valid
+			try {
+				if ($debt->creditor != $this->user && $debt->debtor != $this->user) {
+					throw new Exception('Current user has to be assigned to this debt.');
+				} elseif (($debt->creditor === null || $debt->debtor === null) && $debt->customFriendName == "") {
+					throw new Exception('You have to set creditor and debtor, or one of them has to be customFriendName. Check if the ids are valid.');
+				} elseif (($debt->creditor != null && $debt->debtor != null) && $debt->customFriendName != "") {
+					throw new Exception('You can not set creditor, debtor and customFriendName');
+				} elseif ($debt->amount != "" && ($debt->currency === null || $debt->thingName != "")) {
+					throw new Exception('If amount is chosen, you have to chose currencyId and thingName must be empty. Also, check if currency id is valid.');
+				} elseif ($debt->thingName != "" && ($debt->amount != "" || $debt->currency != null)) {
+					throw new Exception('If you chose to owe a thing, currency and amount must not be set.');
+				} elseif ($debt->amount == "" && $debt->thingName == "") {
+					throw new Exception('You have to set either amount or thingName');
+				} elseif ($debt->amount == "" && $debt->thingName == "") {
+					throw new Exception('You have to set either amount or thingName');
+				}
+			} catch (Exception $e) {
+				$this->sendErrorResponse('Debt ' . $id . ': ' . $e->getMessage());
+			}
+
+			$this->orm->debts->persistAndFlush($debt);
+		}
+
+		$this->sendSuccessResponse($this->getDebtsArray());
+	}
+
+	private function getDebtsArray($which = null) {
 		$toPay = [];
 		$toGet = [];
 
@@ -33,114 +139,14 @@ class DebtsPresenter extends BaseApiPresenter {
 		}
 
 		if ($which == 'toPay') {
-			$this->sendSuccessResponse(['debtsToPay' => $toPay]);
+			return ['debtsToPay' => $toPay];
 		} elseif ($which == 'toGet') {
-			$this->sendSuccessResponse(['debtsToGet' => $toGet]);
+			return ['debtsToGet' => $toGet];
 		} else {
-			$this->sendSuccessResponse(['debts' => array_merge($toPay, $toGet)]);
+			return ['debts' => array_merge($toPay, $toGet)];
 		}
 	}
 
-	/**
-	 * Updates newest debts and shows debt's most recent version
-	 */
-	public function actionUpdate() {
-
-		$id = $this->data['id'];
-
-		// Check the most important things which are nessesary for further actions
-		try {
-			// If id is < than 0, it means that it's a new debt and a new row must be created
-			if (!isset($id) || $id < 0) {
-				$debt = new Debt();
-			} else {
-				$debt = $this->orm->debts->getById($id);
-			}
-
-			if ($debt === null) {
-				throw new Exception('This debt does not exist.');
-			}
-
-			if ($this->data['createdAt'] == "") {
-				throw new Exception('CreatedAt has to be set.');
-			}
-
-			if ($this->data['modifiedAt'] == "") {
-				throw new Exception('ModifiedAt has to be set.');
-			}
-
-			if ($this->data['version'] == "") {
-				throw new Exception('Version has to be an int.');
-			}
-
-		} catch (Exception $e) {
-			$this->sendErrorResponse($e->getMessage());
-		}
-
-
-		// Check if the recieved debt is the most recent version
-		if ($id > 0) {
-			// If the recieved debt is older, send newest version
-			// in case of versions being equal, send server version so that there aren't useless database updates
-			if ((int) $this->data['version'] <= $debt->version) {
-				$this->sendDebtSuccessResponse($debt);
-			}
-		}
-
-		$paidAt = null;
-		$deletedAt = null;
-		if ($this->data['paidAt'] != "") {
-			$paidAt = DateTime::from($this->data['paidAt']);
-		}
-
-		if ($this->data['deletedAt'] != "") {
-			$deletedAt = DateTime::from($this->data['deletedAt']);
-		}
-
-		// Here we have the newest debt, let's update the databse
-
-		$debt->creditor = $this->orm->users->getById($this->data['creditorId']);
-		$debt->debtor = $this->orm->users->getById($this->data['debtorId']);
-		$debt->customFriendName = $this->data['customFriendName'];
-		$debt->amount = $this->data['amount'];
-		$debt->currency = $this->orm->users->getById($this->data['currencyId']);
-		$debt->thingName = $this->data['thingName'];
-		$debt->note = $this->data['note'];
-		$debt->paidAt = $paidAt;
-		$debt->deletedAt = $deletedAt;
-		$debt->createdAt = DateTime::from($this->data['createdAt']);
-		$debt->modifiedAt = new DateTime();
-		$debt->version = (int) $this->data['version'];
-
-		// Check if the debt is valid
-		try {
-			if ($debt->creditor != $this->user && $debt->debtor != $this->user) {
-				throw new Exception('Current user has to be assigned to this debt.');
-			} elseif (($debt->creditor === null || $debt->debtor === null) && $debt->customFriendName == "") {
-				throw new Exception('You have to set creditor and debtor, or one of them has to be customFriendName.');
-			} elseif (($debt->creditor != null && $debt->debtor != null) && $debt->customFriendName != "") {
-				throw new Exception('You can not set creditor, debtor and customFriendName');
-			} elseif ($debt->amount != "" && ($debt->currency === null || $debt->thingName != "")) {
-				throw new Exception('If amount is chosen, you have to chose currencyId and thingName must be empty');
-			} elseif ($debt->thingName != "" && ($debt->amount != "" || $debt->currency != null)) {
-				throw new Exception('If you chose to owe a thing, currency and amount must not be set.');
-			} elseif ($debt->amount == "" && $debt->thingName == "") {
-				throw new Exception('You have to set either amount or thingName');
-			} elseif ($debt->amount == "" && $debt->thingName == "") {
-				throw new Exception('You have to set either amount or thingName');
-			}
-		} catch (Exception $e) {
-			$this->sendErrorResponse($e->getMessage());
-		}
-
-		$debt = $this->orm->debts->persistAndFlush($debt);
-
-		$this->sendDebtSuccessResponse($debt);
-	}
-
-	private function sendDebtSuccessResponse(Debt $debt) {
-		$this->sendSuccessResponse($this->convertDebtToArray($debt));
-	}
 
 	/**
 	 * Converts debt entity to an array, which is suitable for api response
